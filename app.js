5e0a75a430493261786b33abb7d0842329fb413b
rudisiona <rsmageo@gmail.com>
2025-04-07 20:49:54 -0700

    1-5 complete

--- app.js
+++ app.js
@@ -1,36 +1,66 @@
-/*
-Exercise 1: maxOfTwoNumbers()
-
-In this exercise, create a function named maxOfTwoNumbers. 
-It should take two numbers as inputs and return the larger number. 
-If they're equal, return either one.
-
-Exercise 1 has been completed for you:
-*/
+// Excercise 1
 
 const maxOfTwoNumbers = (x, y) => {
-    if (x >= y) {
-      return x;
-    } else {
-      return y;
-    }
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
   }
-  
-  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
-  
-  /*
-Exercise 2: isAdult()
-
-Write a function named isAdult. It should take an age (number) 
-and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
-
-Example: isAdult(21) should return 'Adult'.
-
-Complete the exercise in the space below:
-*/
-
-function isAdult(number){
-    if 
 }
 
-console.log('Exercise 2 Result:', isAdult(21));
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+
+// Exercise 2
+
+function isAdult(age){
+if (age >= 18) {
+  return 'Adult'
+} else {
+  return 'Minor'
+}
+}
+
+console.log('Exercise 2 Result:', isAdult(21))
+
+
+
+// Exercise 3
+
+function isCharAVowel(character){
+  if (character == "a" || character == "e" || character == "i" || character == "o" || character == "u"){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+
+//Exercise 4
+
+function generateEmail(name, domain) {
+  return `${name}@${domain}`
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+
+// Exercise 5
+
+function greetUser(name, timeOfDay,){
+if (timeOfDay === "morning") {
+  return `Good morning, ${name}`
+} else if (timeOfDay === "afternoon") {
+ return `Good afternoon, ${name}`
+} else if (timeOfDay === "night") {
+  return `Good night, ${name}`
+}
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"))
